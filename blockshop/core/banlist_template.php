d04fb4920a444cb66019c5ab8b2cba7a121e168d
Maxim <defilak@mail.ru>
2021-07-04 14:17:56 +0300

    fix banlist

---
+++ blockshop/core/banlist_template.php
@@ -0,0 +1,28 @@
+<?php
+
+//$s = array('Medieval', 'Imphar'); ///массив серверов(первое по умолчанию)
+
+
+$stmt = $pdo->prepare("SELECT * FROM `{$banlist}_{$s[$server_id]}` LIMIT 25");
+$stmt->execute();
+
+$result = $stmt->fetchAll();
+
+?>
+<table class="banlist_table">
+    <tr>
+        <th>Ник</th>
+        <th>Причина</th>
+        <th>Кто забанил?</th>
+        <th>Дата разбана</th>
+    </tr>
+
+    <? foreach ($result as $row) { ?>
+        <tr>
+            <td><?= $row['name'] ?></td>
+            <td><?= $row['reason'] ?></td>
+            <td><?= $row['admin'] ?></td>
+            <td><?= $row['time'] ?></td>
+        </tr>
+    <? } ?>
+</table>
