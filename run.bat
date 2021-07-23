3a08cb438853e89fb4b45193ad59aff30024e898
Maxim <defilak@mail.ru>
2021-07-24 00:43:47 +0300

    update run script (not tested)

--- run.bat
+++ run.bat
@@ -1,1 +1,2 @@
-C:\php\7.4.16\php.exe -S localhost:8000 __server.php
+start sass --watch blockshop/assets/css/style.scss blockshop/assets/css/style.css
+start C:\php\7.4.16\php.exe -S localhost:8000 __server.php
