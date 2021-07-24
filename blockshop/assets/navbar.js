a7de8825c232eac5fdae540303f0cd6deeeb7527
Maxim <defilak@mail.ru>
2021-07-25 00:32:51 +0300

    update navbar script

--- blockshop/assets/navbar.js
+++ blockshop/assets/navbar.js
@@ -1,110 +1,32 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
-    const containerEl = document.getElementById(containerElId)
+    navbar = Object.assign(navbar, {
+        pageContainer: document.getElementById(containerElId),
 
-    async function makePost(url, key, value = 0) {
-        return fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: key + '=' + value,
-        })
-    }
-    navbar.page = async function (page, value = 0) {
-        let response = await makePost(urlIndex, page, value)
-        if (response.ok) {
-            let data = await response.text()
-            containerEl.innerHTML = data
-        }
-    }
+        makePost: async function (url, key, value = 0) {
+            return fetch(url, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                body: key + '=' + value,
+            })
+        },
 
-    navbar.pageAjax = async function (page, value = 0) {
-        let response = await makePost(urlAjax, page, value)
-        if (response.ok) {
-            let data = await response.text()
-            containerEl.innerHTML = data
-        }
-    }
+        page: async function (page, value = 0, url = urlIndex) {
+            let response = await this.makePost(url, page, value)
+            if (response.ok) {
+                let data = await response.text()
+                document.getElementById(containerElId).innerHTML = data
+            }
+        },
 
-    navbar.currency = async function (el) {
-        if (el.value == 0) {
-            el.value = 1
-        } else {
-            el.value = 0
-        }
+        currency: async function (el) {
+            if (el.dataset.value == 0) {
+                el.dataset.value = 1
+            } else {
+                el.dataset.value = 0
+            }
 
-        el.title = await (await makePost(urlAjax, 'balance', el.value)).text()
-        el.src = urlRoot + 'assets/img/' + el.value + '.png'
-    }
+            el.title = await (await this.makePost(urlAjax, 'balance', el.dataset.value)).text()
+            el.querySelector('img').src = urlRoot + 'assets/img/navbar/' + el.dataset.value + '.png'
+        },
+    })
 })
-
-function tolc() {
-    post('lk=0', containerElId, urlIndex)
-}
-
-function setbanlistT() {
-    banlistmode = true
-    navbar.banlistMode = true
-}
-
-function setbanlistF() {
-    banlistmode = false
-    navbar.banlistMode = false
-}
-
-/*function tobanlist() {
-    document.getElementById(resultElId).innerHTML = "";
-    var v1 = document.getElementById("server").value;
-    post("banlist=" + v1, containerElId, urlIndex);
-}*/
-/*
-function toserver() {
-    if (banlistmode) {
-        tobanlist();
-    } else {
-        document.getElementById(resultElId).innerHTML = "";
-        var v1 = document.getElementById("server").value;
-        var v2 = document.getElementById("category").value;
-        post("shop=" + v1 + ":" + v2, containerElId, urlIndex);
-    }
-}*/
-function post2(s1) {
-    var req = new XMLHttpRequest()
-    if (req) {
-        req.open('POST', urlAjax, false)
-        req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-        req.send('balance=' + s1)
-        if (req.status == 200) {
-            var rData = req.responseText
-            return rData
-        } else {
-            alert('Не удалось получить данные:\n' + req.statusText)
-        }
-    } else {
-        alert('Браузер не поддерживает AJAX')
-    }
-}
-
-function valuta() {
-    var div = document.getElementById('changeval')
-    if (div.value == 0) {
-        div.value++
-    } else {
-        div.value--
-    }
-    div.title = post2(div.value)
-    var img = urlRoot + 'assets/img/' + div.value + '.png'
-    div.src = img
-}
-
-//copied
-function props(s1) {
-    document.getElementById(resultElId).innerHTML = ''
-    var var1 = document.getElementById('usercheck').value
-    post(s1 + '=' + var1, containerElId, urlAjax)
-}
-
-function bedit(s1) {
-    document.getElementById(resultElId).innerHTML = ''
-    post(s1, containerElId, urlAjax)
-}
