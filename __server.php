fb38395b7211784fa35f912a806f15a7db131d4a
Maxim <defilak@mail.ru>
2021-07-24 20:25:10 +0300

    update server router

--- __server.php
+++ __server.php
@@ -4,19 +4,24 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+function str_starts_with(string $haystack, string $needle): bool
+{
+    $length = strlen($needle);
+    return substr($haystack, 0, $length) === $needle;
+}
+
 $fiximgqueryroute = explode('?', $_SERVER["REQUEST_URI"]);
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|ico|js)$/', $fiximgqueryroute[0])) {
     return false;    // сервер возвращает файлы напрямую.
 }
 
-if($_SERVER['REQUEST_URI'] === '/') {
+
+if (in_array($_SERVER['REQUEST_URI'], ['/', 'index.php'])) {
     include_once 'index.php';
     exit;
 }
 
-if (file_exists(dirname(__FILE__).$_SERVER['REQUEST_URI'])) {
-    include_once dirname(__FILE__).$_SERVER['REQUEST_URI'];
-} else {
-    include_once 'index.php';
-    exit;
+//load files
+if (file_exists(dirname(__FILE__) . $_SERVER['REQUEST_URI'])) {
+    include_once dirname(__FILE__) . $_SERVER['REQUEST_URI'];
 }
