a68efd1cb51bbe64c4b0168b3654bdc76900b80f
Maxim <defilak@mail.ru>
2021-07-24 20:22:32 +0300

    adding cooldown utility class

--- blockshop/ajax/cooldown.php
+++ blockshop/ajax/cooldown.php
@@ -1,16 +1,41 @@
 <?php
 
-namespace cooldown;
+class Cooldown
+{
+    public $currentTime;
 
-use responses;
+    public function __construct($time)
+    {
+        $this->currentTime = $time;
+        if (empty($_SESSION['buytime']))
+            $_SESSION['buytime'] = 0;
+    }
 
-function set_timer() {
+    public function update($addition_time): void
+    {
+        $_SESSION['buytime'] = $this->currentTime + $addition_time;
+    }
 
+    /**
+     * @return boolean true if on cooldown
+     */
+    public function check(): bool
+    {
+        return $_SESSION['buytime'] > $this->currentTime;
+    }
+
+    public function remaining(): int
+    {
+        return $_SESSION['buytime'] - $this->currentTime;
+    }
 }
 
+
+return new Cooldown(time());
+
 //ставим кулдаун на действия
-$time = time();
-if ($args_count > 1) {
+/*$time = time();
+if ($args_count > 1) { //аргумент может быть только один
     $_SESSION['buytime'] = $time + 60;
     responses\badly("Фриз тебе на одну минуту за такие дела!");
 }
@@ -18,3 +43,18 @@ if ($args_count > 1) {
 if (empty($_SESSION['buytime'])) {
     $_SESSION['buytime'] = 0;
 }
+
+
+function uptime($s1)
+{
+    global $_SESSION;
+    $_SESSION['buytime'] = time() + $s1;
+}
+
+function check()
+{
+    if ($_SESSION['buytime'] > $time) {
+        $tm = skl($_SESSION['buytime'] - $time, array('секунду', 'секунды', 'секунд'));
+        responses\badly("До следующей операции подождите <b>{$tm}</b>!");
+    }
+}*/
