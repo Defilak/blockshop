ea01887793bff81a78a441a3d1506cbd1f7a163a
Maxim <defilak@mail.ru>
2021-07-24 21:00:09 +0300

    update cooldown

--- blockshop/ajax/cooldown.php
+++ blockshop/ajax/cooldown.php
@@ -29,32 +29,3 @@ class Cooldown
         return $_SESSION['buytime'] - $this->currentTime;
     }
 }
-
-
-return new Cooldown(time());
-
-//ставим кулдаун на действия
-/*$time = time();
-if ($args_count > 1) { //аргумент может быть только один
-    $_SESSION['buytime'] = $time + 60;
-    responses\badly("Фриз тебе на одну минуту за такие дела!");
-}
-
-if (empty($_SESSION['buytime'])) {
-    $_SESSION['buytime'] = 0;
-}
-
-
-function uptime($s1)
-{
-    global $_SESSION;
-    $_SESSION['buytime'] = time() + $s1;
-}
-
-function check()
-{
-    if ($_SESSION['buytime'] > $time) {
-        $tm = skl($_SESSION['buytime'] - $time, array('секунду', 'секунды', 'секунд'));
-        responses\badly("До следующей операции подождите <b>{$tm}</b>!");
-    }
-}*/
